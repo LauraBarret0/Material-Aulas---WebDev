e3f58398245c9d44bf4289df4b7352496ffed3b4
Laura Barreto <barreto.laura023@gmail.com>
2025-08-14 22:31:45 -0300

    Aula 2

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,61 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+// Every time window load
+window.onload = function(){
+    showPost()
+
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+// Create
+function addPost(e){
+    e.preventDefault()
+
+    const textPost = document.querySelector('#postText').value
+    const categotyPost = document.querySelector('#postCategory').value
+    const imagePost = document.querySelector('#postImage').value
+
+    const newPost = {
+        text: textPost,
+        category: categotyPost,
+        image: imagePost,
+        date: new Date().toLocaleString()
+    }
+
+    posts.unshift(newPost)
+    document.querySelector("#postForm").reset() 
+    showPost()
+}
+
+// Read
+function showPost(){
+    // Get element where tweets would be insert
+    const listPostEl = document.querySelector('#postList')
+    listPostEl.innerHTML = ''
+
+    // Passando pelo array e criando um twwet para cada um
+    posts.forEach( item =>{
+        const cardPost = document.createElement('div') //Creating element
+        cardPost.classList.add('card-post')
+
+        cardPost.innerHTML = `
+
+            <h2>${item.text}</h2>
+            <img src='${item.image}'/>
+            <h5>Categoria: ${item.category}</h5>
+            <h6>Data e Hora: ${item.date}</h6>
+            <button>Editar</button>
+            <button>Apagar</button>
+        `
+        listPostEl.append(cardPost)
+    })
+
+}
+
+// Update
+function editPost(){}
+
+// Delete
+function deletePost(){}
