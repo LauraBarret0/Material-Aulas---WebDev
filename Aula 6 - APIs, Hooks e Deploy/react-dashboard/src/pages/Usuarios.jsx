e5e0fc30534b1931c6105a0f54770aa3db77425a
Laura Barreto <barreto.laura023@gmail.com>
2025-09-11 22:30:00 -0300

    Aula06

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function Usuarios() {
+
+  const [usuarios, setUsuarios] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUsuarios(data))
+      .catch(erro => console.log(erro))
+      .finally(console.log('Carregamento Finalizado!'))
+  }, [])
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Usuários</h1>
-      <p className="mt-2 text-gray-600">Página de usuários.</p>
+      {usuarios.map(pegaItem => (
+        <p>{pegaItem.name}</p>
+      ))}
     </div>
   );
 }
