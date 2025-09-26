a805a61b0504463754bea90b2b77f28a459dc6f3
Laura Barreto <barreto.laura023@gmail.com>
2025-09-25 22:33:33 -0300

    Aula07

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
@@ -3,11 +3,16 @@ import { GraficoPizza } from "@/components/GraficoPizza";
 import { Button } from "@/components/ui/button"
 
 export default function Relatorios() {
+  
+  function handleClick(){
+    alert('Hey Laurett')
+  }
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Relatórios</h1>
       <p className="mt-2 text-gray-600">Página de relatórios.</p>
-      <Button>Click me</Button>
+      <Button onClick={handleClick}>Click me</Button>
       <div className="grid grid-cols-2">
         <GraficoBarras/>
         <GraficoPizza/>
